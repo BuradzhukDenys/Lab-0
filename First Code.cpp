1ac5732c09a1e6733356e39192d5f7d5ddb6211d
Denys Buradzhuk <buradzhuk.denys.clg@chnu.edu.ua>
2024-09-11 17:28:07 +0200

    Додав меню для вибору заповнення масиву, та спосіб заповнення масиву з клавіатури

--- First Code.cpp
+++ First Code.cpp
@@ -3,28 +3,74 @@
 
 using namespace std;
 
+void ShowMenu()
+{
+    //Панель меню
+    cout << "Choose the way to fill the array: " << endl;
+    cout << "1. Fill the array with ranom numbers." << endl;
+    cout << "2. Fill the array with numbers from keyboard." << endl;
+    cout << "0. Exit." << endl;
+}
+
+void FillRandom(int *Arr, int size)
+{
+    //Заповнюємо масив випадковими числами від -10 до 10
+    for (int i = 0; i < size; i++)
+    {
+        Arr[i] = rand() % 21 - 10;
+    }
+}
+
+void FillWithKeyboard(int *Arr, int size)
+{
+    //Заповнюємо масив числами з клавіатури
+    for (int i = 0; i < size; i++)
+    {
+        cin >> Arr[i];
+    }
+}
+
 void ShowArray(int *Arr, int size)
 {
+    //Вивід масиву
     for (int i = 0; i < size; i++)
     {
         cout << "Arr[" << i << "] = " << Arr[i] << endl;
     }
 }
 
+void Menu(int *Arr, int size)
+{
+    //Вибираємо спосіб заповнення масиву
+    ShowMenu();
+    int option;
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+        FillRandom(Arr, size);
+        ShowArray(Arr, size);
+        break;
+        
+    case 2:
+        FillWithKeyboard(Arr, size);
+        ShowArray(Arr, size);
+        break;
+    
+    case 0:
+        break;
+    
+    default:
+        break;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
-    int n; //Вводимо розмір масиву
+    int size; //Вводимо розмір масиву
     cout << "Enter a size of the array: ";
-    cin >> n;
-    int Arr[n];
-
-    //Заповнюємо масив випадковими числами від -10 до 10
-    for (int i = 0; i < n; i++)
-    {
-        Arr[i] = rand() % 21 - 10;
-    }
-
-    //Виводимо масив
-    ShowArray(Arr, n);
+    cin >> size;
+    int Arr[size];
+    Menu(Arr, size);
 }
